bbe1d12b7729bc045a496210113879a16185f545
yf <125532795+89yf@users.noreply.github.com>
2024-01-31 10:49:15 +0800

    Update round0.py

--- round0.py
+++ round0.py
@@ -1,4 +1,4 @@
-# Task 1
+# ----- Task 1 -----
 x = int(input())
 y = int(input())
 z = int(input())
@@ -6,4 +6,43 @@ z = int(input())
 fee = x + y*z*12
 print(fee)
 
-# Task 2
+# ----- Task 2 -----
+money = int(input())
+cost = int(input())
+
+no_rides = money//cost # % gives remainder & // gives quotient
+print(no_rides)
+
+# ----- Task 3 -----
+msg = str(input())
+
+d = str(msg[2:] + msg[:2])
+print(d)
+
+# ----- Task 4 -----
+f = str(input())
+s = str(input())
+t = str(input())
+
+if f == s and s == t:
+  print("OK")
+elif f != s and f != t:
+  print("BOTH MISMATCH")
+elif f != s:
+  print("ENTRY 2 MISMATCH")
+elif f != t:
+  print("ENTRY 3 MISMATCH")
+
+# ----- Task 5 ----- 2/3 testcase1: runtime error
+drink = str(input())
+if drink == 't' or 'c':
+  add = int(input())
+else:
+  add = 0
+total = 1 + add
+if total >= 10:
+  print("10 is the maximum!")
+else:
+  print(total)
+
+# ----- Task 6 -----
